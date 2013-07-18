0187db0c5d125f9d19f6b1bdf4416bbf6d9e6eeb
Victor <graaff@brixcrm.nl>
2013-07-18 18:06:53 +0200

    Set root folder to /data/tomcat, created a testform for AnswerQuestions

--- src/main/java/nl/utwente/db/kickinquest/server/AnswerQuestionsServlet.java
+++ src/main/java/nl/utwente/db/kickinquest/server/AnswerQuestionsServlet.java
@@ -1,26 +1,64 @@
 package nl.utwente.db.kickinquest.server;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import nl.utwente.db.neogeo.utils.FileUtils;
+import nl.utwente.db.neogeo.utils.StringUtils;
 
 @MultipartConfig
 public class AnswerQuestionsServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    public final static String ROOT_FOLDER = "/data/tomcat/";
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // TODO: implement this
-    	throw new RuntimeException("To be implemented");
+    	throw new RuntimeException("Invalid request, use POST instead.");
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        doGet(request, response);
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String dataType = request.getParameter("dataType");
+        String teamId = request.getParameter("teamId");
+        String deviceId = request.getParameter("deviceId");
+        Part data = getFile(request);
+        
+    	String fileName = storeData(dataType, teamId, deviceId, data);
+    	PrintWriter writer = response.getWriter();
+    	
+    	writer.write(new File(fileName).getCanonicalPath());
     }
+    
+    protected String storeData(String dataType, String teamId, String deviceId, Part data) throws IOException {
+		String folderPath = ROOT_FOLDER + teamId + "/" + deviceId;
+		File directory = new File(folderPath);
+		
+		directory.mkdirs();
+		
+		String fileName = folderPath + "/" + dataType + "-" + (System.currentTimeMillis() / 1000) + ".txt";
+		String dataAsString = StringUtils.inputStreamToString(data.getInputStream());
+		
+		FileUtils.writeFile(fileName, dataAsString);
+		
+		return fileName;
+	}
+
+	protected Part getFile(HttpServletRequest request) {
+		try {
+			return request.getPart("data");
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to load attachment", e);
+		} catch (ServletException e) {
+			throw new RuntimeException("Unable to load attachment", e);
+		}
+	}
 }
